59a14596b1de5be22e8a7256a8bb7c17fff60e03
cozzz <zeco11320@163.com>
2018-12-05 17:37:48 +0800

    change cookie pkg

--- controller/router.go
+++ controller/router.go
@@ -30,6 +30,7 @@ import (
 	"github.com/charlesmade/blogofgv/theme"
 	"github.com/charlesmade/blogofgv/util"
 	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
@@ -61,7 +62,7 @@ func MapRoutes() *gin.Engine {
 
 	ret.Use(gin.Recovery())
 
-	store := sessions.NewCookieStore([]byte(model.Conf.SessionSecret))
+	store := cookie.NewStore([]byte(model.Conf.SessionSecret))
 	store.Options(sessions.Options{
 		Path:     "/",
 		MaxAge:   model.Conf.SessionMaxAge,
